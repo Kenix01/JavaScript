2e51a6ee0fb01ee76276b8d631985a0b58ae15ca
Kenix01 <i.i.stefanyuk@gmail.com>
2022-07-28 21:06:54 +0300

    lesson 2

---
+++ lesson_2/script.js
@@ -0,0 +1,133 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+// let arr = [
+//     1,2,3,'Ivan','Stefanyuk',6,7,true,false,10
+// ]
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+// console.log(arr[6])
+// console.log(arr[7])
+// console.log(arr[8])
+// console.log(arr[9])
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+// let book1 = {
+//     title: 'Гаррі Поттер і келих вогню',
+//     pageCount: 200,
+//     genre:'Роман'
+// }
+//
+// let book2 = {
+//     title: 'Прокляття титана',
+//     pageCount: 416,
+//     genre:'Фантастичний роман'
+// }
+// let book3 = {
+//     title: 'Лабіринт смерт',
+//     pageCount: 496,
+//     genre:'Роман'
+// }
+
+// console.log(book1,book2,book3)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+// let book1 = {
+//     title: 'Гаррі Поттер і келих вогню',
+//     pageCount: 200,
+//     genre:'Роман',
+//     authors:[{name:'Дж. К. Ролінґ',age:56}]
+// }
+//
+// let book2 = {
+//     title: 'Прокляття титана',
+//     pageCount: 416,
+//     genre:'Фантастичний роман',
+//     authors:[{name:'Рік Ріордан',age:58}]
+// }
+// let book3 = {
+//     title: 'Лабіринт смерт',
+//     pageCount: 496,
+//     genre:'Роман',
+//     authors:[{name:'Рік Ріордан',age:58}]
+// }
+//
+// console.log(book1,book2,book3)
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+
+let users = [
+    {
+        name:'user1',
+        username:'username1',
+        password:'11'
+    },
+    {
+        name:'user2',
+        username:'username2',
+        password:'22'
+    },
+    {
+        name:'user3',
+        username:'username3',
+        password:'33'
+    },
+    {
+        name:'user4',
+        username:'username4',
+        password:'44'
+    },
+    {
+        name:'user5',
+        username:'username5',
+        password:'55'
+    },
+    {
+        name:'user6',
+        username:'username6',
+        password:'66'
+    },
+    {
+        name:'user7',
+        username:'username7',
+        password:'77'
+    },
+    {
+        name:'user8',
+        username:'username8',
+        password:'88'
+    },
+    {
+        name:'user9',
+        username:'username9',
+        password:'99'
+    },
+    {
+        name:'user10',
+        username:'username10',
+        password:'1010'
+    },
+    {
+        name:'user11',
+        username:'username11',
+        password:'1111'
+    }
+]
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)
+console.log(users[10].password)
